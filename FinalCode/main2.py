f4d158d99806526f18e4cc1a7514bc425a882424
mariasalgado1106 <mariasalgado1106@gmail.com>
2026-02-16 11:45:09 +0100

    now: -> TADs for everything except through holes

---
+++ FinalCode/main2.py
@@ -0,0 +1,31 @@
+from OCC.Display.SimpleGui import init_display
+import os
+
+from FeatureRecognition.feature_recognition import FeatureRecognition
+from FeatureRecognition.part_visualizer_occ import PartVisualizer_occ
+from FeatureRecognition.part_vizualizer_plotly import Part_Visualizer
+from FeatureRecognition.geometry_analysis import (
+    load_step_file,
+    analyze_shape,
+    print_face_analysis_table,
+    print_edge_analysis_table
+)
+
+def main():
+    # Initialize display
+    display, start_display, add_menu, add_function_to_menu = init_display()
+
+    # Load STEP file
+    my_shape = load_step_file(os.path.join("STEPFiles", "Part3.stp"))
+    if not my_shape:
+        return
+
+
+    # Feature Recognition
+    print("\nPART 3: FEATURE RECOGNITION")
+    recognizer = FeatureRecognition(my_shape)
+    features = recognizer.identify_features()
+    # stats
+    print(f"Detected {len(features)} features")
+    for f in features:
+        print(f["feature_type"], f["node_indices"])
