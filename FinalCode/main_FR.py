542a36ac0f7714b44b96af26b28c93fe399378b1
mariasalgado1106 <mariasalgado1106@gmail.com>
2026-02-18 15:28:36 +0100

    next: -> dependence mapping? -> minimum setup -> intermidiate parts -> research more about setup components, fixture

--- FinalCode/main_FR.py
+++ FinalCode/main_FR.py
@@ -12,7 +12,7 @@ def main():
     display, start_display, add_menu, add_function_to_menu = init_display()
 
     # Load STEP file
-    my_shape = load_step_file(os.path.join("STEPFiles", "Part3.stp"))
+    my_shape = load_step_file(os.path.join("STEPFiles", "example4.stp"))
     if not my_shape:
         return
 
